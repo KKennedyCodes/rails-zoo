992afb3596b9b77e2ed88aaca80e4e64b3ff3d74
KKennedyCodes <holmstedtk@gmail.com>
2019-10-06 08:54:01 -0700

    Added Initial Test, Passing

--- test/controllers/animals_controller_test.rb
+++ test/controllers/animals_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe AnimalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  
+  it "should get index" do
+    get "/animals"
+    must_respond_with :success
+  end
+  
 end
