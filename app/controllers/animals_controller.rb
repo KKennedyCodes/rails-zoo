19ec93ff15f2c1a7ee6d8e79ef50cd18d3deed7a
KKennedyCodes <holmstedtk@gmail.com>
2019-10-06 09:16:11 -0700

    Added Show View

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,27 @@
+ANIMALS = [
+{name: "Bobo", species: "Monkey", age: 4, sex: "M", notes: "Likes Ice Cream", img: "http://lorempixel.com/200/200/animals/"}
+]
+
 class AnimalsController < ApplicationController
+  def index
+    @animals = ANIMALS
+  end
+  
+  def show
+    animal_id = params[:id].to_i
+    @animal = ANIMALS[animal_id]
+    if @animal.nil?
+      head :not_found
+      return
+    end
+  end
+  
+  def edit
+  end
+  
+  def find
+  end
+  
+  def destroy
+  end
 end
